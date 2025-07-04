d0cead29e724d0fd56a3cb4cafeba584a6996c39
drbythedragon <dantecarpio15@gmail.com>
2025-07-03 20:09:06 -0500

    Update Qwen_batch_20250703_vbp5w2qrg.bat

--- Qwen_batch_20250703_vbp5w2qrg.bat
+++ Qwen_batch_20250703_vbp5w2qrg.bat
@@ -1,27 +1,33 @@
 @echo off
-:: Batch file to disable UAC (User Account Control)
-
-echo.
-echo ==============================
-echo  Disabling User Account Control
-echo ==============================
-echo.
+:: Batch script with auto-elevation to run with admin rights
 
 :: Check if user is admin
 net session >nul 2>&1
-if %errorLevel% NEQ 0 (
-    echo This script must be run as Administrator!
-    pause
-    exit /b
+if %errorLevel% == 0 (
+    echo Running with admin rights.
+    goto AdminSection
+) else (
+    echo Requesting administrative privileges...
+    goto UACPrompt
 )
 
-:: Disable UAC
+:UACPrompt
+:: Create VBScript to trigger UAC prompt
+echo Set UAC = CreateObject("Shell.Application") > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /b
+
+:AdminSection
+:: Clean up the temporary VBScript
+if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
+
+:: Your elevated commands go below this line
+echo Successfully running with elevated permissions!
+
+:: Example: Disable UAC
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v EnableLUA /t REG_DWORD /d 0 /f >nul 2>&1
+echo UAC has been disabled. You may need to restart your computer.
 
-echo.
-echo UAC has been disabled.
-echo Your computer will now restart to apply changes.
-echo.
-
-timeout /t 5 >nul
-shutdown /r /t 0
+:: Pause so user can see output
+pause
